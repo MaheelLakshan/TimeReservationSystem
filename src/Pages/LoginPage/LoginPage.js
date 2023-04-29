a967f566cc810722ad5194f6ec0ecf1ea96f0a45
Maheel <Maheellakshanra@gmail.com>
2023-04-29 23:27:26 +0530

    responsive part was done for loginpage

--- src/Pages/LoginPage/LoginPage.js
+++ src/Pages/LoginPage/LoginPage.js
@@ -7,13 +7,15 @@ import {
   faGoogle,
   faTwitter,
 } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faLock, faUser } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 
 function LoginPage() {
   // const [signUpClassName, setSignUpClassName] = useState('container');
   // const [logInClassName, setLogInClassName] = useState('container');
   const [toggleClassName, setToggleClassName] = useState('container');
+
+  const handlerloginto = () => {};
 
   const handleLogIn = () => {
     setToggleClassName('container log-in-mode');
@@ -36,10 +38,10 @@ function LoginPage() {
   return (
     <div className={toggleClassName}>
       <div className="signin-signup">
-        <form action="" className="signin-form">
-          <h2 className="title">Log In</h2>
+        <form action="" className="log-in-form">
+          <div className="title">RESERVE SLOTS</div>
           <div className="input-field">
-            <FontAwesomeIcon icon={faLock} className="lll" />
+            <FontAwesomeIcon icon={faUser} className="lll" />
             <input type="text" className="mytext" placeholder="Username" />
           </div>
           <div className="input-field">
@@ -47,7 +49,9 @@ function LoginPage() {
             <input type="password" placeholder="Password" />
           </div>
           {/* <input type="submit" value={login} className="btn" /> */}
-          <div className="btn">LoG IN</div>
+          <button className="btn" onClick={handlerloginto}>
+            Log IN
+          </button>
           <p className="social-text">OR sign in with social platform</p>
           <div className="social-media">
             <Link href="#" className="social-icon">
@@ -60,18 +64,20 @@ function LoginPage() {
               <FontAwesomeIcon icon={faTwitter} />
             </Link>
           </div>
-          <p className="accout-text">
-            Do not have an Account..?{' '}
-            <a href="#" id="sign-up-btn2" onClick={handlerSignup}>
-              Sign Up
-            </a>
-          </p>
+          <div className="accout-text">
+            Do not have an Account?{' '}
+            <div style={{ justifyContent: 'center', display: 'flex' }}>
+              <a href="#" id="sign-up-btn2" onClick={handlerSignup}>
+                Sign Up
+              </a>
+            </div>
+          </div>
         </form>
 
-        <form action="" className="signup-form">
-          <h2 className="title">Sign up</h2>
+        <form action="" className="sign-up-form">
+          <div className="title">SIGN UP</div>
           <div className="input-field">
-            <FontAwesomeIcon icon={faLock} className="lll" />
+            <FontAwesomeIcon icon={faUser} className="lll" />
             <input type="text" className="mytext" placeholder="Username" />
           </div>
           <div className="input-field">
@@ -96,19 +102,24 @@ function LoginPage() {
               <FontAwesomeIcon icon={faTwitter} />
             </Link>
           </div>
-          <p className="accout-text">
-            Already have an Account..?{' '}
-            <a href="#" id="log-in-btn2" onClick={handlerLogIn}>
-              Log In
-            </a>
-          </p>
+          <div className="accout-text">
+            Already have an Account?{' '}
+            <div style={{ justifyContent: 'center', display: 'flex' }}>
+              <a href="/" id="log-in-btn2" onClick={handlerLogIn}>
+                Log In
+              </a>
+            </div>
+          </div>
         </form>
       </div>
       <div className="panels-container">
         <div className="panel left-panel">
           <div className="content">
-            <h3>Member of Brand?</h3>
-            <p>asdadadsadad</p>
+            <h3>Member of Platform?</h3>
+            <div className="para">
+              if you already have and account click here to shift to the log
+              in!!
+            </div>
             <button className="btn" id="log-in-btn" onClick={handleLogIn}>
               Log IN
             </button>
@@ -118,8 +129,12 @@ function LoginPage() {
 
         <div className="panel right-panel">
           <div className="content">
-            <h3>New to Brand?</h3>
-            <p>asdadadsadad</p>
+            <h3>New to Platform?</h3>
+            <div className="para">
+              {' '}
+              if you are new to our platform, 1st register with your credential
+              here!!
+            </div>
             <button className="btn" id="sign-up-btn" onClick={handleSignUp}>
               Sign Up
             </button>
