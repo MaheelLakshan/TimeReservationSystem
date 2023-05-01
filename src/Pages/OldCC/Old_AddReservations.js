72812100d57342ea6543f498c98a137bb0e196d1
SCRanaweera <ranaweerasc65@gmail.com>
2023-05-01 21:14:28 +0530

    Formating | AddReservation | NewCC,OldCC,SeminarRm

--- src/Pages/OldCC/Old_AddReservations.js
+++ src/Pages/OldCC/Old_AddReservations.js
@@ -2,131 +2,12 @@ import React, { useState } from 'react';
 import '../CommonTemplate.css';
 import Old_SideBar from '../../Components/SlideBar/Old_SideBar';
 import PlacesBar from '../../Components/PlacesBar/PlacesBar';
-import { Container, Row, Col, Form, FormGroup, Button } from 'reactstrap';
 import './../../styles/addreservation.css';
-
-function AddReservations() {
-  const [name, setName] = useState('');
-  const [date, setDate] = useState('');
-  const [from, setFrom] = useState('');
-  const [to, setTo] = useState('');
-  const [details, setDetails] = useState('');
-  const [isRecurring, setIsRecurring] = useState(false);
-  const [endDate, setEndDate] = useState('');
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (isRecurring) {
-      // Make recurring reservations
-      let currDate = new Date(date);
-      let end = new Date(endDate);
-      while (currDate <= end) {
-        // Make a reservation for the current date
-        // code to make the reservation goes here
-        currDate.setDate(currDate.getDate() + 7); // Increment the date by 7 days for weekly recurrence
-      }
-    } else {
-      // Make a one-time reservation
-      // code to make the reservation goes here
-    }
-  };
-
-  return (
-    <div className="Common">
-      <div className="CommonGlass">
-      <Old_SideBar />
-        <div className="MainDash">
-          <PlacesBar />
-
-          <form onSubmit={handleSubmit}>
-            <div className="form-group">
-              <label htmlFor="name">Name:</label>
-              <input
-                type="text"
-                id="name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="date">Date:</label>
-              <input
-                type="date"
-                id="date"
-                value={date}
-                onChange={(e) => setDate(e.target.value)}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="from">From:</label>
-              <input
-                type="time"
-                id="from"
-                value={from}
-                onChange={(e) => setFrom(e.target.value)}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="to">To:</label>
-              <input
-                type="time"
-                id="to"
-                value={to}
-                onChange={(e) => setTo(e.target.value)}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="details">Details:</label>
-              <textarea
-                id="details"
-                value={details}
-                onChange={(e) => setDetails(e.target.value)}
-              ></textarea>
-            </div>
-            <div className="form-group">
-              <label htmlFor="isRecurring">Make weekly recurring reservation here:</label>
-              <input
-                type="checkbox"
-                id="isRecurring"
-                checked={isRecurring}
-                onChange={(e) => setIsRecurring(e.target.checked)}
-              />
-            </div>
-            {isRecurring && (
-              <div className="form-group">
-                <label htmlFor="endDate">End date:</label>
-                <input
-                  type="date"
-                  id="endDate"
-                  value={endDate}
-                  onChange={(e) => setEndDate(e.target.value)}
-                />
-              </div>
-            )}
-            <div className="button-container">
-            <button type="submit">Add Reservation</button>
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-export default AddReservations;
-
-
-
-/*import React, { useState } from 'react';
-import '../CommonTemplate.css';
-import Old_SideBar from '../../Components/SlideBar/Old_SideBar';
-import PlacesBar from '../../Components/PlacesBar/PlacesBar';
 import { Container, Row, Col, Form, FormGroup, Button } from 'reactstrap';
-import './../../styles/addreservation.css';
-import Old_FeaturesBar from '../../Components/FeaturesBar/Old_FeaturesBar';
+import FeaturesBar from '../../Components/FeaturesBar/FeaturesBar';
 
-function AddReservations() {
-  const [name, setName] = useState('');
+function Old_AddReservations() {
+  const [title, setTitle] = useState('');
   const [date, setDate] = useState('');
   const [from, setFrom] = useState('');
   const [to, setTo] = useState('');
@@ -157,18 +38,28 @@ function AddReservations() {
         <Old_SideBar />
         <div className="MainDash">
           <PlacesBar />
-
-          <form onSubmit={handleSubmit}>
-            <div className="form-group">
-              <label htmlFor="name">Name:</label>
+          <h3
+              style={{
+                paddingLeft: '20px',
+                color: '#fcc81f',
+                background: '#6B6E70',
+                marginBottom: '10px',
+              }}
+            >
+              Make a reservation in Old CC
+            </h3>
+            <div className="formadd">
+          <Form onSubmit={handleSubmit}>
+            <div className="formgroupadd">
+              <label htmlFor="title">Title:</label>
               <input
                 type="text"
-                id="name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
+                id="title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
               />
             </div>
-            <div className="form-group">
+            <div className="formgroupadd">
               <label htmlFor="date">Date:</label>
               <input
                 type="date"
@@ -177,7 +68,7 @@ function AddReservations() {
                 onChange={(e) => setDate(e.target.value)}
               />
             </div>
-            <div className="form-group">
+            <div className="formgroupadd">
               <label htmlFor="from">From:</label>
               <input
                 type="time"
@@ -186,7 +77,7 @@ function AddReservations() {
                 onChange={(e) => setFrom(e.target.value)}
               />
             </div>
-            <div className="form-group">
+            <div className="formgroupadd">
               <label htmlFor="to">To:</label>
               <input
                 type="time"
@@ -195,7 +86,7 @@ function AddReservations() {
                 onChange={(e) => setTo(e.target.value)}
               />
             </div>
-            <div className="form-group">
+            <div className="formgroupadd">
               <label htmlFor="details">Details:</label>
               <textarea
                 id="details"
@@ -203,16 +94,47 @@ function AddReservations() {
                 onChange={(e) => setDetails(e.target.value)}
               ></textarea>
             </div>
-            <div className="button-container">
-              <button type="submit">Add Reservation</button>
+            <div className="formgroupadd">
+              <label htmlFor="isRecurring">Make weekly recurring reservation here:</label>
+              <input
+                type="checkbox"
+                id="isRecurring"
+                checked={isRecurring}
+                onChange={(e) => setIsRecurring(e.target.checked)}
+              />
             </div>
-          </form>
+            {isRecurring && (
+              <div className="formgroupadd">
+                <label htmlFor="endDate">End date:</label>
+                <input
+                  type="date"
+                  id="endDate"
+                  value={endDate}
+                  onChange={(e) => setEndDate(e.target.value)}
+                />
+              </div>
+            )}
+           
+
+            <Button className="btn_addreservation">Add Reservation</Button>
+
+
+          </Form>
+          </div>
         </div>
-        <Old_FeaturesBar />
+
+        <FeaturesBar />
       </div>
     </div>
   );
 }
 
-export default AddReservations;
-*/
+
+export default Old_AddReservations;
+
+
+
+
+
+
+
