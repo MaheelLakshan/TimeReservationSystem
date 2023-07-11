700329a888ea6231d9a707fe756d20af2f04391a
SCRanaweera <ranaweerasc65@gmail.com>
2023-07-12 02:39:33 +0530

    changes in about page

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './About.css';
 import HeaderNew from '../../Components/Header/HeaderNew';
+import Modal from 'react-modal';
 
 import maheelImage from '../../Assets/maheel.jpg';
 import anupamaImage from '../../Assets/anupama.jpg';
@@ -8,36 +9,61 @@ import jeewanthaImage from '../../Assets/jeewantha.jpg';
 import sachiniImage from '../../Assets/sachini.jpg';
 
 function CheckPlaces() {
+  const [selectedDeveloper, setSelectedDeveloper] = useState(null);
+
+  const developers = [
+    {
+      name: 'Lakshan R.A.M.',
+      image: maheelImage,
+      description: '3rd year undergraduate',
+      phone: '011223366'
+    },
+    {
+      name: 'Morapitiya M.A.V.',
+      image: anupamaImage,
+      description: '3rd year undergraduate',
+      phone: '011223366'
+    },
+    {
+      name: 'Jeewanatha K.H.C',
+      image: jeewanthaImage,
+      description: '3rd year undergraduate',
+      phone: '011223366'
+    },
+    {
+      name: 'Ranaweera S.C.',
+      image: sachiniImage,
+      description: '3rd year undergraduate',
+      phone: '011223366'
+    },
+  ];
+
+  const handleDeveloperClick = (index) => {
+    setSelectedDeveloper(developers[index]);
+  };
+
+  const closeModal = () => {
+    setSelectedDeveloper(null);
+  };
+
   return (
     <div className="AboutBackground">
       <HeaderNew />
 
       <div className="TeamSection">
         <h2 className="TeamTitle">Meet the Team Behind the Online Reservation System for the IS Department</h2>
-        
       </div>
 
       <div className="DeveloperPhotos">
-        <div className="DeveloperPhoto">
-          <img src={maheelImage} alt="Maheel" />
-          <div className="DeveloperName">Lakshan R.A.M.</div>
-        </div>
-        <div className="DeveloperPhoto">
-          <img src={anupamaImage} alt="Anupama" />
-          <div className="DeveloperName">Morapitiya M.A.V.</div>
-        </div>
-        <div className="DeveloperPhoto">
-          <img src={jeewanthaImage} alt="Jeewantha" />
-          <div className="DeveloperName">Jeewanatha K.H.C</div>
-        </div>
-        <div className="DeveloperPhoto">
-          <img src={sachiniImage} alt="Sachini" />
-          <div className="DeveloperName">Ranaweera S.C.</div>
-        </div>
+        {developers.map((developer, index) => (
+          <div className="DeveloperPhoto" key={index} onClick={() => handleDeveloperClick(index)}>
+            <img src={developer.image} alt={developer.name} />
+            <div className="DeveloperName">{developer.name}</div>
+          </div>
+        ))}
       </div>
 
       <div className="TeamSection">
-        
         <h3 className="TeamDescription">
           We are a group of developers who are passionate about making it easier for students and faculty to reserve common facilities in the IS department.
           Our system is easy to use and secure, and it allows you to reserve a facility with just a few clicks.
@@ -48,9 +74,20 @@ function CheckPlaces() {
         <h3 className="TeamDescription">Contact Us - 0775858582</h3>
       </div>
 
-
-      
-
+      <Modal
+        isOpen={selectedDeveloper !== null}
+        onRequestClose={closeModal}
+        className="DeveloperModal"
+        overlayClassName="DeveloperModalOverlay"
+      >
+        <img src={selectedDeveloper?.image} alt={selectedDeveloper?.name} className="DeveloperModalImage" />
+        <div className="DeveloperInfo">{selectedDeveloper?.name}</div>
+        <div className="DeveloperInfo">{selectedDeveloper?.description}</div>
+        <div className="DeveloperInfo">{selectedDeveloper?.phone}</div>
+        <button className="CloseButton" onClick={closeModal}>
+          Close
+        </button>
+      </Modal>
     </div>
   );
 }
