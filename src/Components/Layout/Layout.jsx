887d34a765d4f98a7ae6eee1085d97a7be4cc82a
SCRanaweera <ranaweerasc65@gmail.com>
2023-08-19 08:47:10 +0530

    changes in components

---
+++ src/Components/Layout/Layout.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+import Footer from '../Footer/Footer';
+import Routers from '../../router/Routers';
+const Layout = () => {
+  return (
+    <>
+
+    <Routers />
+    <Footer />
+
+    </>
+  );
+};
+
+export default Layout;
